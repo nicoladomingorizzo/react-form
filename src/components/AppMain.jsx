e720ee62da51427e9e11bdbffcfcc9c5b0e66b0f
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-07-18 17:00:30 +0200

    changed all tasks in objects

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,35 +1,43 @@
 import { useState } from 'react';
 
 export default function AppMain({ objectList }) {
-    const [newTask, setNewTask] = useState('Aggiungi Nuova Task');
-    const [tasks, setTasks] = useState(objectList);
+    const [newObject, setNewObject] = useState('Aggiungi un nuovo oggetto');
+    const [objects, setObjects] = useState(objectList);
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(newTask);
-        if (newTask === "" || '') {
+        console.log(newObject);
+        if (newObject === "" || '') {
             alert('Il campo è vuoto, impossibile salvare inserire una task')
         };
-        setTasks([...tasks, newTask]);
+        setObjects([...objects, newObject]);
     };
 
     function handleClickRemove(id) {
-        // tasks.splice(index, 1);
-        setTasks(prev => prev.filter((task) => task.id !== id));
+        // objects.splice(index, 1);
+        setObjects(prev => prev.filter((task) => task.id !== id));
     };
 
     function handleClickEdit(id) {
-        //     setTasks(prev => prev.map((task) => task.id === id));
+        // setObjects(prev => prev.map(task, title => {
+
+        //     if (task.id === id) {
+        //         task.title = '<h1>Lorem ipsum dolor sit amet.</h1>'
+        //         // <input className='form-control  py-3 ps-4 ms-3' placeholder={title} type="text" value={setObjects} onChange={e => setNewObject(e.target.value)} />
+        //     }
+        //     return task
+        // }
+        // ))
     };
 
     return (
         <>
             <ul className="list-group list-unstyled d-flex justify-content-between">
-                {tasks.map((task, index) => {
+                {objects.map(task => {
                     return (
                         <li key={task.id} className='list-group-item py-3 ps-4 ms-3 d-flex justify-content-between align-items-center'>{task.title}
                             <div>
-                                <button className='btn' onClick={() => handleClickEdit(index)}><i className='bi bi-pencil px-1'></i></button><button className='btn' onClick={() => handleClickRemove(index)}><i className='bi bi-trash px-1'></i></button>
+                                <button className='btn' onClick={() => handleClickEdit(task.id)}><i className='bi bi-pencil px-1'></i></button><button className='btn' onClick={() => handleClickRemove(task.id)}><i className='bi bi-trash px-1'></i></button>
                             </div>
                         </li>
                     );
@@ -37,7 +45,7 @@ export default function AppMain({ objectList }) {
             </ul>
             <form onSubmit={handleSubmit}>
                 <div className='d-flex'>
-                    <input className='form-control mt-2 py-2 ps-4 ms-3' placeholder='Inserisci una nuova Cosa da fare' type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
+                    <input className='form-control mt-2 py-2 ps-4 ms-3' placeholder='Inserisci una nuova Cosa da fare' type="text" value={newObject} onChange={e => setNewObject(e.target.value)} />
                     <button className='btn' type='submit'><i className='bi bi-floppy'>Salva</i></button>
                 </div>
             </form>
