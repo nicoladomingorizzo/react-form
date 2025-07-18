ff4896a69e2311826ec4e2c6248a4cb112debdd8
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-07-18 12:33:07 +0200

    updated handleClickRemove to remove tasks

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -7,21 +7,25 @@ export default function AppMain({ todoList }) {
     function handleSubmit(e) {
         e.preventDefault();
         console.log(newTask);
-        setTasks([...tasks, newTask])
+        setTasks([...tasks, newTask]);
+    }
+    function handleClickRemove(index) {
+        tasks.splice(index, 1);
+        setTasks([...tasks]);
     }
 
     return (
         <>
-            <ul className="list-group list-unstyled">
+            <ul className="list-group list-unstyled d-flex justify-content-between">
                 {tasks.map((task, index) => {
                     return (
-                        <li key={index} className='list-group-item'>{task}</li>
+                        <li key={index} className='list-group-item py-3 ps-4 ms-3'>{task}<button className='btn mx-2' onClick={() => handleClickRemove(index)}><i className='bi bi-trash px-1'></i></button></li>
                     )
                 })}
             </ul>
             <form onSubmit={handleSubmit}>
                 <div className='d-flex'>
-                    <input className='form-control mt-2' placeholder='Inserisci una nuova Cosa da fare' type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
+                    <input className='form-control mt-2 py-2 ps-4 ms-3' placeholder='Inserisci una nuova Cosa da fare' type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
                     <button className='btn' type='submit'><i className='bi bi-floppy'>Salva</i></button>
                 </div>
             </form>
@@ -29,10 +33,6 @@ export default function AppMain({ todoList }) {
     );
 };
 
-// Milestone 1
-// Creare una pagina che visualizzi una lista di articoli, mostrandone solo il titolo.
-// Milestone 2
-// Aggiungiamo in pagina un semplice form con un campo input in cui inserire il titolo di un nuovo articolo del blog. Al submit del form, mostrare la lista degli articoli aggiornati.
 // BONUS
 // Aggiungere la possibilità di cancellare ciascun articolo utilizzando un'icona.
 // Implementare la funzionalità di modifica del titolo di un post.
